d51cd0f25eebb0335896ca92b1033ab55ec4fe65
JoonPyoJang <jjpyo1009@gmail.com>
2022-11-15 09:26:58 +0900

    feat: add test code

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
 
 # Create your tests here.
+
+class UserLoginAPITestCase(APITestCase):
+    def test_registration(self):
+        url = reverse("sign-up")
+        user_data={
+            "username" : "jjpyo1009",
+            "password" : "123",
+            "email" : "jjpyo1009@asd.asd",
+            "fullname" : "jangjoonpyo",
+        }
+        response = self.client.post(url, user_data)
+        self.assertEqual(response.data, {'message': '가입 완료!!'})
